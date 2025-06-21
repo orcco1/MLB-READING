2dfce9baff3e3e601313ef88d4da4da0593380c4
Adrian Alejandro Cruz Alanis <alejandrocruzalanis@gmail.com>
2025-06-20 18:39:34 -0600

    obtain MLB Teams Data

--- teams.py
+++ teams.py
@@ -1,5 +1,24 @@
-### teams.py
+import statsapi
+import pandas as pd
 
-import statsapi as mlbstats
+# Obtain MLB teams
+teams = statsapi.get('teams', {'sportIds': 1})
 
-print(mlbstats.standings(leagueId="103,104", division="all", include_wildcard=True, season=None, standingsTypes=None, date=None))
+# Format data
+teams_data = [
+    {
+        'ID': team['id'],
+        'Name': team['name'],
+        'Abreviation': team['abbreviation'],
+        'City': team['locationName'],
+        'League': team['league']['name'],
+        'Division': team['division']['name']
+    }
+    for team in teams['teams']
+]
+
+# Create dataframe
+df = pd.DataFrame(teams_data)
+
+# Save CSV
+df.to_csv('mlb-teams.csv', index=False)
